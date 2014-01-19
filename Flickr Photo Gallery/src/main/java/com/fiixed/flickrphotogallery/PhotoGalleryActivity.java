422facb517f2a5ad7edc8f6680e2ff4514cc0fdb
Andrew Bell <fiixed@gmail.com>
2014-01-19 15:52:27 +1100

    initial commit

---
+++ Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryActivity.java
@@ -0,0 +1,21 @@
+package com.fiixed.flickrphotogallery;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+
+}
