c0e2f17a3998b047de82182c71069e8c614c98f7
Andrew Bell <fiixed@gmail.com>
2014-01-20 07:54:19 +1100

    added skeleton AsynTask

--- Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryFragment.java
+++ Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryFragment.java
@@ -1,16 +1,22 @@
 package com.fiixed.flickrphotogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by abell on 1/19/14.
  */
 public class PhotoGalleryFragment extends Fragment {
+
+    public static final String TAG = "PhotoGalleryFragment";
 
     GridView mGridView;
 
@@ -19,6 +25,7 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -28,5 +35,19 @@ public class PhotoGalleryFragment extends Fragment {
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetcher().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
