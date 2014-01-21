b905ee50124dc4ccd34487d4a5449dc3e01b3e12
Andrew Bell <fiixed@gmail.com>
2014-01-21 11:44:22 +1100

    search working

--- Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryActivity.java
+++ Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryActivity.java
@@ -1,9 +1,13 @@
 package com.fiixed.flickrphotogallery;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,9 +17,28 @@ import android.os.Build;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
+    private static final String TAG = "PhotoGalleryActivity";
+
     @Override
     protected Fragment createFragment() {
         return new PhotoGalleryFragment();
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        PhotoGalleryFragment fragment = (PhotoGalleryFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainer);
+
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i(TAG, "Received a new search query" + query);
+        }
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .edit()
+                .putString(FlickrFetcher.PREF_SEARCH_QUERY, null)
+                .commit();
+
+
+        fragment.updateItems();
+    }
 }
--- Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryFragment.java
+++ Flickr Photo Gallery/src/main/java/com/fiixed/flickrphotogallery/PhotoGalleryFragment.java
@@ -1,19 +1,30 @@
 package com.fiixed.flickrphotogallery;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,7 +46,8 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
-        new FetchItemsTask().execute();
+        setHasOptionsMenu(true);
+        updateItems();
 
         mThumbnailThread = new ThumbnailDownloader(new Handler());
         mThumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
@@ -51,6 +63,10 @@ public class PhotoGalleryFragment extends Fragment {
         mThumbnailThread.start();
         mThumbnailThread.getLooper();
         Log.i(TAG, "Background thread started");
+    }
+
+    public void updateItems() {
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -73,6 +89,43 @@ public class PhotoGalleryFragment extends Fragment {
             mGridView.setAdapter(null);
         }
     }
+    @TargetApi(11)
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_photo_gallery, menu);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Pull out the SearchView
+            MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+            SearchView searchView = (SearchView)searchItem.getActionView();
+
+            // Get the data from our searchable.xml as a SearchableInfo
+            SearchManager searchManager = (SearchManager)getActivity()
+                    .getSystemService(Context.SEARCH_SERVICE);
+            ComponentName name = getActivity().getComponentName();
+            SearchableInfo searchInfo = searchManager.getSearchableInfo(name);
+
+            searchView.setSearchableInfo(searchInfo);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_search:
+                getActivity().onSearchRequested();
+                return true;
+            case R.id.menu_item_clear:
+                PreferenceManager.getDefaultSharedPreferences(getActivity())
+                        .edit()
+                        .putString(FlickrFetcher.PREF_SEARCH_QUERY, null)
+                        .commit();
+                        updateItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     public void onDestroy() {
@@ -91,7 +144,17 @@ public class PhotoGalleryFragment extends Fragment {
 
         @Override
         protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            Activity activity = getActivity();
+            if(activity == null) {
+                return new ArrayList<GalleryItem>();
+            }
+            String query = PreferenceManager.getDefaultSharedPreferences(activity)
+                    .getString(FlickrFetcher.PREF_SEARCH_QUERY, null);
+            if(query != null) {
+                return new FlickrFetcher().search(query);
+            } else {
             return new FlickrFetcher().fetchItems();
+            }
         }
 
         @Override
